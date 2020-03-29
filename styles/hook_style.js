d28462f4c2c1944b645240c6632c7e160e84f396
Nwani Victory <vickywane@gmail.com>
2020-03-29 01:28:36 +0100

    changes

---
+++ styles/hook_style.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+
+const useWindowWidth: any = () => {
+  const [Width, setWidth] = useState < number > null;
+
+  setTimeout(function() {
+    setWidth(window.innerWidth);
+  }, 1000);
+
+  const handleResize = value => {
+    setWidth(value);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize.bind(this));
+    return () => window.removeEventListener("resize", handleResize.bind(this));
+  }, []);
+  return Width;
+};
+
+export default useWindowWidth;
